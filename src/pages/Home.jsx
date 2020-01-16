9e2ab0327c9c201cbbdb1b650acf29721d1190ce
Julian <Julian@mbp15-jromana.home>
2020-01-16 09:49:45 +0100

    add eslint and styled components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+`
+
+const array = [ '1',2, ]
+
+const Home = () => {
+  return (
+    <Title> Hello World</Title>
+  )
+}
+
+export default Home
