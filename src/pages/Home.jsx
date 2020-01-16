c5d9c32afcb5cd94ab6a632865cf9a3136fd3a6c
Julian <Julian@mbp15-jromana.home>
2020-01-16 16:07:07 +0100

    rebase from master

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  color: red;
-`
-
 const Home = () => {
+
+  const Title = styled.h1` color: red;`
   return (
     <>
       <Title> Hello World</Title>
